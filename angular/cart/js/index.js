53ba247dc41ea128c670dcc7207cbfebe80999f4
sunshine1125 <371262808@qq.com>
2017-11-21 17:46:41 +0800

    date

--- angular/cart/js/index.js
+++ angular/cart/js/index.js
@@ -93,6 +93,6 @@ app.directive('copyRight', function(){
     return {
       restrict: 'E',
       replace: 'false',
-      template: '<h5>Copyright &copy;2017</h5>'
+      template: `<h5>Copyright &copy;${new Date().getFullYear()}</h5>`
     }
 });
